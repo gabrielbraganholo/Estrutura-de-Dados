8a238c5f35b923340b654ff94c9aec7d7cc9f32b
Gabriel Morais Braganholo <gabrielmbraganholo@gmail.com>
2026-05-13 10:09:56 -0300

    minor changes

--- aulas/aula_8/recursaoJAVA/Recursao.java
+++ aulas/aula_8/recursaoJAVA/Recursao.java
@@ -1,4 +1,68 @@
 package aula_8.recursaoJAVA;
 
+import aula_7.MatrizesEspeciais.Dados;
+
+import java.util.ArrayList;
+
 public class Recursao {
+
+    static void mostrarSequenciaDecrescenteR(int numero){
+        if(numero >= 0){
+            System.out.println(numero);
+            mostrarSequenciaDecrescenteR(numero - 1);
+        }
+    }
+
+    static void mostrarSequenciaCrescenteR(int numero){
+        if(numero >= 0){
+            mostrarSequenciaCrescenteR(numero - 1);
+            System.out.println(numero);
+        }
+    }
+
+    static void mostrarIntervaloR(int ini, int fim){
+        if (ini <= fim){
+            System.out.println(ini);
+            mostrarIntervaloR(ini + 1, fim);
+        }
+    }
+
+    static void mostrarListaR(ArrayList<Integer> lista, int tamanhoLista){
+        if (tamanhoLista > 0){
+            mostrarListaR(lista, tamanhoLista - 1);
+            if(lista.get(tamanhoLista - 1) % 2 == 0){
+                System.out.println(lista.get(tamanhoLista - 1));
+            }
+        }
+    }
+
+    static void subistiruirR(ArrayList<Integer> lista, int tamanhoLista, int substituido, int escolhido){
+        if (tamanhoLista > 0){
+            subistiruirR(lista, tamanhoLista - 1, substituido, escolhido);
+            if(lista.get(tamanhoLista - 1) == substituido){
+                System.out.println(escolhido);
+            }
+            else{
+                System.out.println(lista.get(tamanhoLista - 1));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int numero = 5;
+        // mostrarSequenciaCrescente(numero);   // mostrar a sequencia de 0 até o número
+        // mostrarSequenciaDecrescente(numero); // mostrar a sequencia do número até o 0
+
+        //mostrarIntervaloR(3, 6);
+
+        ArrayList<Integer> lista = new ArrayList<>();
+
+        lista.add(30);
+        lista.add(41);
+        lista.add(50);
+
+        //mostrarListaR(lista, lista.size());
+
+        subistiruirR(lista, lista.size(), 30, 100);
+    }
 }
--- aulas/aula_9/continuacaoRecursaoJAVA/Recursao.java
+++ aulas/aula_9/continuacaoRecursaoJAVA/Recursao.java
@@ -1,4 +1,46 @@
 package aula_9.continuacaoRecursaoJAVA;
 
+// Recursão é um recurso computacional de programação que substitui as instruções de repetição
+// Toda repetição obedece a 3 pontos:
+// 1 - Inicialização de variável de controle
+// 2 - Teste de parada com a variável de controle
+// 3 - Transformação da variável de controole
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Recursao {
+    public static void  popular(ArrayList<Integer> lista, int quantidade){
+        Random random = new Random();
+        int numero;
+
+        for(int i = 0; i < quantidade; i++){
+            numero = random.nextInt(100);
+            lista.add(i, numero);
+        }
+    }
+
+    public static void exibir(ArrayList<Integer> lista){
+        for (Integer i : lista) {
+            System.out.println(i);
+        }
+    }
+
+    public static void exibirR(ArrayList<Integer> lista, int tamanho){
+        if (tamanho > 0) {
+            // código antes do empilhamento
+            //System.out.println(lista.get(tamanho -1);     -> printa de trás pra frente da lista
+            exibirR(lista, tamanho - 1);
+            //código depois do empilhamento
+            System.out.println(lista.get(tamanho - 1));  // -> printa na ordem da lista
+        }
+    }
+
+    static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<>();
+        int quantidade = 5;
+        popular(lista, quantidade);
+        //exibir(lista);
+        exibirR(lista, lista.size());
+    }
 }
