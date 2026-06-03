54e34bd741101cd2dba7cb7c9d7df3b2ece95679
Gabriel Morais Braganholo <gabrielmbraganholo@gmail.com>
2026-06-03 13:42:15 -0300

    minor changes

--- aulas/aula_12/desafio/Main.java
+++ aulas/aula_12/desafio/Main.java
@@ -2,6 +2,7 @@ package aula_12.desafio;
 
 public class Main {
     public static void main(String[] args) {
+        // Troque para o caminho adequado do arquivo caso tente rodar em outra máquina
         String base = "C:/arquivos github/Estrutura-de-Dados/aulas/aula_12/desafio/mapa.csv";
 
         Grafo grafo = Utilidade.extrairArestas(base);
