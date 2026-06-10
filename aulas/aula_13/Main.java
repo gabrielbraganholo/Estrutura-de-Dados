6e1a4bdd60398e9e9b436406370b816e6ae56f29
Gabriel Morais Braganholo <gabrielmbraganholo@gmail.com>
2026-06-10 08:46:38 -0300

    aula_13

---
+++ aulas/aula_13/Main.java
@@ -0,0 +1,15 @@
+package aula_13;
+
+public class Main {
+    public static void main(String[] args) {
+        // Troque para o caminho adequado do arquivo caso tente rodar em outra máquina
+        String base = "C:/arquivos github/Estrutura-de-Dados/aulas/aula_13/mapa.csv";
+
+        Grafo grafo = Utilidade.extrairArestas(base);
+
+        if (grafo != null) {
+            System.out.println("--------------------Grafo--------------------");
+            grafo.mostrarGrafo();
+        }
+    }
+}
---
+++ aulas/aula_13/Utilidade.java
@@ -0,0 +1,65 @@
+package aula_13;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class Utilidade {
+    /**
+     * Função estatica extrai os vértices de um arquivo
+     * @param base base de dados a ser extraída
+     * @return retorna os vértices extraídos da base
+     */
+    static ArrayList<String> extrairVertices(String base) {
+        ArrayList<String> vertices = new ArrayList<>();
+
+        try (BufferedReader leitor = new BufferedReader(new FileReader(base, StandardCharsets.UTF_8))) {
+
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+                String[] vertice = linha.split(",");
+
+                if (!vertices.contains(vertice[0])) {
+                    vertices.add(vertice[0]);
+                } else if (!vertices.contains(vertice[1])) {
+                    vertices.add(vertice[1]);
+                }
+            }
+
+            vertices.sort(String::compareTo);
+
+        } catch (Exception e) {
+            System.err.println("Erro: " + e.getMessage());
+            System.exit(1);
+        }
+
+        return vertices;
+    }
+
+    /**
+     * Função estática que extrai as arestas de um arquivo
+     * @param base base de dados a ser extraída
+     * @return retorna um grafo com os devidos vertices e arestas
+     */
+    static Grafo extrairArestas(String base) {
+        ArrayList<String> vertices = new ArrayList<>();
+        Grafo grafo = new Grafo(extrairVertices(base));
+
+        try (BufferedReader leitor = new BufferedReader(new FileReader(base, StandardCharsets.UTF_8))) {
+
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+                String[] vertice = linha.split(",");
+
+                grafo.inserirAresta(grafo.pegarIndice(vertice[0]), grafo.pegarIndice(vertice[1]), Integer.parseInt(vertice[2]));
+            }
+
+        } catch (Exception e) {
+            System.err.println("Erro: " + e.getMessage());
+            return null;
+        }
+
+        return grafo;
+    }
+}
